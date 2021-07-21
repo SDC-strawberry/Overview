5f4b3a0e5fd0ec25580967d6ea56286cc788b1b7
Sam Brown <baudot@gmail.com>
2021-07-21 16:30:42 -0700

    Add second query and fix new bug in first query.

--- db/db.js
+++ db/db.js
@@ -17,12 +17,27 @@ class Db {
   }
 
   getProducts(res, req, getProductsReplyCallback, page, count) {
+    console.log('page', page);
+    console.log('count', count);
+    let minId = (parseInt(page) - 1) * parseInt(count);
+    console.log('minId: ', minId);
     let query = `select * from products
-      where id > ${(page - 1) * count}
+      where id > ${minId}
+      order by id asc
       limit ${count};`
+    console.log('query: ', query);
     pool.query(query, (err, data) => {
       if (err) { console.log('Error: ', err); }
       getProductsReplyCallback(err, data.rows, res, req);
+    });
+  }
+
+  getProductById(res, req, getProductByIdReplyCallback, productId) {
+    let query = `select * from products
+      where id = ${productId};`
+    pool.query(query, (err, data) => {
+      if (err) { console.log('Error: ', err); }
+      getProductByIdReplyCallback(err, data.rows, res, req);
     });
   }
 }
--- server.js
+++ server.js
@@ -2,25 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const app = express()
 const port = 3000
-const Db = require('./db/db.js')
+const db = require('./db/db.js')
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded());
 
-const getProductsReplyCallback = function(err, data, res, req) {
+const generalReplyCallback = function(err, data, res, req) {
   if (err) { res.send(err); }
   else res.send(data);
 }
 
 app.get('/', (req, res) => {
-  Db.testQuery();
+  db.testQuery();
   res.send('Hello World');
 });
 
 app.get('/products', (req, res) => {
   let page = req.query.page;
   let count = req.query.count;
-  Db.getProducts(res, req, getProductsReplyCallback, page, count);
+  db.getProducts(res, req, generalReplyCallback, page, count);
+});
+
+app.get('/products/:productId', (req, res) => {
+  db.getProductById(res, req, generalReplyCallback, req.params.productId);
 });
 
 app.listen(port, () => {
