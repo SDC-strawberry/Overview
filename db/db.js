046d9bb4201ed2269fa3ca9216fba6ecb34e0645
Sam Brown <baudot@gmail.com>
2021-07-20 19:09:46 -0700

    Dotenv added, testing a real query.

--- db/db.js
+++ db/db.js
@@ -1,9 +1,15 @@
-console.log('Included')
+console.log('process.env.PGDATABASE', process.env.PG_DATABASE);
 const { Pool } = require('pg');
 
-const pool = new Pool();
+const pool = new Pool({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT
+});
 
-pool.query('SELECT NOW()', (err, res) => {
+pool.query('select * from products where id=5;', (err, res) => {
   console.log('Error: ', err);
   console.log('Response: ', res);
 });
