f022bc8e61d2488e0a9bd6b3d408b86112d65080
Sam Brown <baudot@gmail.com>
2021-08-09 19:18:24 -0700

    Last version of test run in load testing locally.

--- k6tests/test.js
+++ k6tests/test.js
@@ -3,13 +3,23 @@ import { check, sleep } from 'k6';
 
 export let options = {
   stages: [
-    {duration: '30s', target: 20 },
-    {duration: '1m30s', target: 10 },
-    {duration: '20s', target: 0 },
+    {duration: '5s', target: 5000 },
+    {duration: '14s', target: 5000 },
+    {duration: '3s', target: 0 },
   ],
 };
 
+let url;
+let randId;
+let randPage;
+
 export default function() {
-  var url = 'http://localhost:3000/products/1';
+  randId = Math.floor(Math.random() * 10000);
+  randPage = Math.floor(Math.random() * 1000);
+//  url = `http://localhost:3000/products/${randId}`;
+  url = `http://localhost:3000/products?page=${randPage}&count=5`;
+//  url = `http://localhost:3000/products/${randId}/styles`;
+//  url = `http://localhost:3000/products/${randId}/related`;
   http.get(url);
+  sleep(1);
 }
